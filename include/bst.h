5195a0d2cef9dfe1ea84c3e1616243e8f7be350e
PudochkinaAnastasia <116427670+Pudochkina@users.noreply.github.com>
2023-05-25 00:33:40 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -1,66 +1,125 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
-
-template<typename T>
+template <typename T>
 class BST {
- private:
-  struct Node {
-  T value;
-  int count=0;
-  Node* left = nullptr;
-  Node* right = nullptr;
-  };
-  Node* root;
-  Node* addNode(Node*, T&) {
-if (root == nullptr) {
-    root = new Node();
-root->value = val;
-root->count = 1;
-root->left = root->right = nullptr;
-  }
-else if (root->value < val) 
-  root->left = addNode(root->left, val);
-else if (root->value > val) 
-  root->right = addNode(root->right, val);
-else 
-  root->count++;
-return root;
-}
-int searchNode(Node* root, const T& val) {
-if (root == nullptr) 
-  return 0;
-else if (root->value == val) 
-  return root->count;
-else if (root->value < val) 
-  return searchNode(root->left, val);
-else 
-  return searchNode(root->right, val);
-}
-int calc_l(Node* root) {
-  int left = 0, right = 0;
-if (root == nullptr)
-  return 0;
-else {
-  left = calc_l(root->left);
-  right = calc_l(root->right);
-}
-if (left > right) 
-  return ++left;
-else 
-  return ++right;
-}
  public:
-BST() :root(nullptr) {}
-void add(const T& val) {
-  root = addNode(root, val);
-}
-int search(const T& val) {
-  return searchNode(root, val);
-}
-int calc() {
-  return calc_l(root) - 1;
-}
-};
+    struct Node {
+        T data;
+        int count;
+        Node* right;
+        Node* left;
+    };
 
+ private:
+    Node* root;
+    Node* addNode(Node* localRoot, T data) {
+        if (localRoot == nullptr) {
+            localRoot = new Node;
+            localRoot->data = data;
+            localRoot->count = 1;
+            localRoot->left = localRoot->right = nullptr;
+        } else if (localRoot->data.compare(data) > 0) {
+            localRoot->left = addNode(localRoot->left, data);
+        } else if (localRoot->data.compare(data) < 0) {
+            localRoot->right = addNode(localRoot->right, data);
+        } else {
+            localRoot->count++;
+        }
+        return localRoot;
+    }
+    int depthTree(Node* localRoot) {
+        if (localRoot == nullptr) {
+            return 0;
+        } else {
+            int l = depthTree(localRoot->left);
+            int r = depthTree(localRoot->right);
+            return 1 + (l >= r ? l : r);
+        }
+    }
+    int searchNode(Node* localRoot, T data) {
+        int check = localRoot->data.compare(data);
+        if (check == 0) {
+            return localRoot->count;
+        } else if (check > 0) {
+            if (localRoot->left != nullptr) {
+                return searchNode(localRoot->left, data);
+            }
+            return 0;
+        } else {
+            if (localRoot->right != nullptr) {
+                return searchNode(localRoot->right, data);
+            }
+            return 0;
+        }
+    }
+    void delTree(Node* localRoot) {
+        if (localRoot == nullptr) {
+            return;
+        } else {
+            delTree(localRoot->left);
+            delTree(localRoot->right);
+            delete localRoot;
+        }
+    }
+    Node* delNode(Node* localRoot, T data) {
+        Node* p, * v;
+        if (localRoot == nullptr) {
+            return localRoot;
+        } else if (localRoot->value.compare(data) > 0) {
+            localRoot->left = delNode(localRoot->left, data);
+        } else if (localRoot->value.compare(data) < 0) {
+            localRoot->right = delNode(localRoot->right, data);
+        }  else {
+            p = localRoot;
+            if (localRoot->right == nullptr) {
+                localRoot = localRoot->left;
+            } else if (localRoot->left == nullptr) {
+                localRoot = localRoot->right;
+            } else {
+                v = localRoot->left;
+                if (v->right) {
+                    while (v->right->right)
+                        v = v->right;
+                    localRoot->value = v->right->value;
+                    localRoot->count = v->right->count;
+                    p = v->right;
+                    v->right = v->right->left;
+                } else {
+                    localRoot->value = v->value;
+                    localRoot->count = v->count;
+                    p = v;
+                    localRoot->left = localRoot->left->left;
+                }
+            }
+            delete p;
+        }
+        return localRoot;
+    }
+
+ public:
+    BST() : root(nullptr) {}
+    void add(T data) {
+        root = addNode(root, data);
+    }
+    int depth() {
+        if (root) {
+            return depthTree(root) - 1;
+        }
+        return 0;
+    }
+    int search(T data) {
+        return searchNode(root, data);
+    }
+    void clear() {
+        if (root) {
+            delTree(root);
+            root = nullptr;
+        }
+    }
+    void remove(T data) {
+        if (root)
+            root = delNode(root, data);
+    }
+};
 #endif  // INCLUDE_BST_H_
